e202c802ea969ab9c73e1eb02f037c7957e9788f
Doug Riblet <smashzen85@gmail.com>
2016-10-23 18:40:27 -0500

    returning JSON with three values

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 var express = require("express");
-var bodyParser = require('body-parser')
-var get_ip = require('ipware')().get_ip;
     
 var app = express();
 var port = process.env.PORT || 8040;
-
-// app.use(bodyParser.urlencoded({ extended: false }))
- 
-// app.use(bodyParser.json())
  
 app.use(function (req, res) {
-	console.log("=======req.headers: ", req.headers);
-	var ip_info = get_ip(req);
-	console.log("=====ip: ", req.ip);
-	var acceptLanguage = req.headers["accept-language"].split(',');
-	var language = acceptLanguage[0];
-  res.write('Maybe you posted:\n')
-  res.end(JSON.stringify(language + " " + ip_info.clientIp))
+	var userip = req.ip;
+	var language = req.headers["accept-language"].split(',')[0];
+	var agent = req.headers["user-agent"].split(/[\(\)]/)[1];
+  res.end(JSON.stringify({
+  	"ipaddress":userip,
+  	"language":language,
+  	"software":agent
+  }))
 })
-
-// app.get('/', function (req, res) {
-
-//   res.send('GET request to homepage');
-// });
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
