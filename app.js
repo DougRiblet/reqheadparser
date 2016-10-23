4763e1f7f8e52ebb9e8da2728844205b7ced8ea7
Doug Riblet <smashzen85@gmail.com>
2016-10-23 18:49:50 -0500

    use better method for getting user ip

--- app.js
+++ app.js
@@ -4,18 +4,16 @@ var app = express();
 var port = process.env.PORT || 8040;
  
 app.use(function (req, res) {
-	var userip = req.ip;
 	var language = req.headers["accept-language"].split(',')[0];
 	var agent = req.headers["user-agent"].split(/[\(\)]/)[1];
-	var ip2 = req.headers['x-forwarded-for'] || 
+	var userip = req.headers['x-forwarded-for'] || 
      req.connection.remoteAddress || 
      req.socket.remoteAddress ||
      req.connection.socket.remoteAddress;
   res.end(JSON.stringify({
   	"ipaddress":userip,
   	"language":language,
-  	"software":agent,
-  	"ip2":ip2
+  	"software":agent
   }))
 })
 
