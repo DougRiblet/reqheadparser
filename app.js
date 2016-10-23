ea244382dcf209ffc8bf5960c6ed21087bbb3839
Doug Riblet <smashzen85@gmail.com>
2016-10-23 18:14:36 -0500

    uninstall modules

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 var express = require("express");
 var bodyParser = require('body-parser')
-
+var get_ip = require('ipware')().get_ip;
+    
 var app = express();
-var port = 8040;
-// process.env.PORT || 
+var port = process.env.PORT || 8040;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }))
  
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
  
 app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
+	console.log("=======req.headers: ", req.headers);
+	var ip_info = get_ip(req);
+	console.log("=====ip: ", req.ip);
+	var acceptLanguage = req.headers["accept-language"].split(',');
+	var language = acceptLanguage[0];
+  res.write('Maybe you posted:\n')
+  res.end(JSON.stringify(language + " " + ip_info.clientIp))
 })
+
+// app.get('/', function (req, res) {
+
+//   res.send('GET request to homepage');
+// });
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
