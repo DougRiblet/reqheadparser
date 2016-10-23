63f62f0ea848aae35fc9688e56e2053ec1b3ba72
Doug Riblet <smashzen85@gmail.com>
2016-10-23 16:42:17 -0500

    add body-parser and package.json

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require("express");
+var bodyParser = require('body-parser')
 
 var app = express();
-var port = process.env.PORT || 8040;
+var port = 8040;
+// process.env.PORT || 
 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
 
-app.get('/', function(req, res) {
-  
-});
-
-app.get('/favicon.ico', function(req, res) {
-  res.sendStatus(200);
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
